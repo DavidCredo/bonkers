8fa880a69e0d2f2b095a54be025002892027571f
devdev42 <accounts@jona.email>
2022-10-17 03:09:30 +0200

    erste Implmentierung eines Kamerazugriffs

---
+++ lib/Screens/camera_screen.dart
@@ -0,0 +1,54 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:bonkers/main.dart';
+
+class CameraScreen extends StatefulWidget {
+  /// Default Constructor
+  const CameraScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            print('User denied camera access.');
+            break;
+          default:
+            print('Handle other errors.');
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: CameraPreview(controller),
+    );
+  }
+}
