2581af26714c837a73bca6f27fe7903245e30c47
DavidC <davidcredo00@gmail.com>
2022-10-18 21:26:13 +0200

    ListView for scanned items

---
+++ lib/Screens/scanned_item_listview.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ItemsListPage extends StatefulWidget {
+  final String itemsString;
+  const ItemsListPage({super.key, required this.itemsString});
+
+  @override
+  State<ItemsListPage> createState() => _ItemsListPageState();
+}
+
+class _ItemsListPageState extends State<ItemsListPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scanned Items'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return _listItem(index);
+        },
+      ),
+    );
+  }
+
+  Widget _listItem(int index) {
+    return Card(
+      child: ListTile(title: Text("Item")),
+    );
+  }
+}
