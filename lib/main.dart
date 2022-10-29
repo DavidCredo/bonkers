5b9b4f7771d753ff21c5d654119f4336967b8755
devdev42 <accounts@jona.email>
2022-10-29 18:06:34 +0200

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:bonkers/Screens/Authentication/login_screen.dart';
 import 'package:bonkers/Screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod/riverpod.dart';
 import 'firebase_options.dart';
 
 main() async {
