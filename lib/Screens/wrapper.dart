0aedf609a9eafabfea1c68a7cd77a6c02df3b782
devdev42 <accounts@jona.email>
2022-10-29 17:01:34 +0200

    removed unused imports and switched to new HomeView

--- lib/Screens/wrapper.dart
+++ lib/Screens/wrapper.dart
@@ -1,7 +1,5 @@
 import 'package:bonkers/Screens/Authentication/handler.dart';
-import 'package:bonkers/Screens/Authentication/login_screen.dart';
-import 'package:bonkers/Screens/text_recognizer_view.dart';
-import 'package:bonkers/models/firebaseuser.dart';
+import 'package:bonkers/Screens/home_view.dart';
 import 'package:bonkers/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,8 +12,8 @@ class Wrapper extends ConsumerWidget {
     final authStateAsync = ref.watch(authStateChangesProvider);
 
     return authStateAsync.when(
-      data: (user) => user != null ? TextRecognizerView() : Handler(),
-      error: ((error, stackTrace) => Text('Error: $error')),
-      loading: (() => const CircularProgressIndicator()));
+        data: (user) => user != null ? const HomeView() : const Handler(),
+        error: ((error, stackTrace) => Text('Error: $error')),
+        loading: (() => const CircularProgressIndicator()));
   }
 }
