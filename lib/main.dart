a590b17d5340090e88a3671972d1c202f429c55c
DavidC <davidcredo00@gmail.com>
2022-10-29 11:47:22 +0200

    Initial Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:bonkers/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-main() {
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
