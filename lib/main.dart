66cba680cc507166ed0e7968aa2908892294c55d
devdev42 <accounts@jona.email>
2022-10-29 18:10:40 +0200

    changed folder's name from Screens to views

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bonkers/Screens/wrapper.dart';
+import 'package:bonkers/views/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
---
+++ lib/views/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:bonkers/views/Authentication/handler.dart';
+import 'package:bonkers/views/home_view.dart';
+import 'package:bonkers/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Wrapper extends ConsumerWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authStateAsync = ref.watch(authStateChangesProvider);
+
+    return authStateAsync.when(
+        data: (user) => user != null ? const HomeView() : const Handler(),
+        error: ((error, stackTrace) => Text('Error: $error')),
+        loading: (() => const CircularProgressIndicator()));
+  }
+}
