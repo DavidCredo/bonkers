c5ba37d416c81eb9d1608888c97669f531d23221
DavidC <davidcredo00@gmail.com>
2022-11-20 14:56:49 +0100

    implemented fetching bons from db

--- lib/controller/database.dart
+++ lib/controller/database.dart
@@ -1,18 +1,38 @@
 import 'package:bonkers/controller/auth.dart';
+import 'package:bonkers/models/bon.dart';
+import 'package:bonkers/models/bon_list.dart';
 import 'package:bonkers/models/firebaseuser.dart';
 import 'package:bonkers/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final userCollectionProvider = StreamProvider.autoDispose<Map?>((ref) {
+final userCollectionProvider = StreamProvider.autoDispose((ref) {
   final userStream = ref.watch(authStateChangesProvider);
 
   final user = userStream.value;
 
   if (user != null) {
     var docRef = FirebaseFirestore.instance.collection('users').doc(user.uid);
-    return docRef.snapshots().map((doc) => doc.data());
+    return docRef.snapshots();
+  } else {
+    return const Stream.empty();
+  }
+});
+
+final userBonsCollectionProvider = StreamProvider.autoDispose<List<Bon>>((ref) {
+  final userStream = ref.watch(authStateChangesProvider);
+
+  final user = userStream.value;
+
+  if (user != null) {
+    var collRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .collection('Bons');
+    return collRef.snapshots().map((snapShot) => snapShot.docs
+        .map((document) => Bon.fromJson(document.data()))
+        .toList());
   } else {
     return const Stream.empty();
   }
@@ -32,7 +52,7 @@ class DatabaseService {
 
   void getUser(AuthenticatedUser user, FirebaseUser authenticatedUser) async {
     await db.doc(authenticatedUser.uid).get().then((DocumentSnapshot doc) {
-      final userAsJson = doc.data() as Map<String, dynamic>; 
+      final userAsJson = doc.data() as Map<String, dynamic>;
       return AuthenticatedUser.fromJson(userAsJson);
     });
   }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,18 +3,18 @@ import 'package:bonkers/models/bon.dart';
 class AuthenticatedUser {
   final String firstName;
   final String email;
-  final List<Bon>? bons;
+  // final List<Bon>? bons;
 
-  AuthenticatedUser({required this.firstName, required this.email, this.bons});
+  AuthenticatedUser({required this.firstName, required this.email });
 
   factory AuthenticatedUser.fromJson(Map<String, dynamic> data) {
     final firstName = data['firstName'] as String;
     final email = data['email'] as String;
-    final bonsData = data['Bons'] as List<dynamic>?;
-    final bons = bonsData != null
-        ? bonsData.map((bonData) => Bon.fromJson(bonData)).toList()
-        : <Bon>[];
-    return AuthenticatedUser(firstName: firstName, email: email, bons: bons);
+    // final bonsData = data['Bons'] as List<dynamic>?;
+    // final bons = bonsData != null
+    //     ? bonsData.map((bonData) => Bon.fromJson(bonData)).toList()
+    //     : <Bon>[];
+    return AuthenticatedUser(firstName: firstName, email: email );
   }
 
   Map<String, dynamic> toJson() {
