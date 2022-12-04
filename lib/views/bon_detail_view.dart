394e36d039cf903e84d70c35e06f5968942c2a49
DavidC <davidcredo00@gmail.com>
2022-12-04 22:27:23 +0100

    updated formatting of currency & refactor of bon ui

--- lib/views/bon_detail_view.dart
+++ lib/views/bon_detail_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
 import '../models/bon.dart';
 import '../models/bon_item.dart';
@@ -19,6 +20,7 @@ class BonDetailView extends ConsumerStatefulWidget {
 
 class _BonDetailViewState extends ConsumerState<BonDetailView> {
   double? sum;
+  bool didChangePayer = false;
   @override
   void initState() {
     super.initState();
@@ -31,33 +33,51 @@ class _BonDetailViewState extends ConsumerState<BonDetailView> {
   Widget build(BuildContext context) {
     final Bon bon = widget.bon;
     final bonItems = bon.articles;
+    final sumInEuros =
+        NumberFormat.currency(locale: 'eu', symbol: "€").format(sum);
+    final selectedPayer = ref.read(payerNotifierProvider).selectedPayer;
     return Scaffold(
       appBar: AppBar(title: Text(bon.title)),
       body: Column(
         children: <Widget>[
-          Flexible(
-            flex: 4,
-            child: ListView.builder(
-                itemCount: bonItems.length,
-                itemBuilder: ((context, index) => ListTile(
-                    trailing: Text(bonItems[index].payer ?? "Niemand"),
-                    title: Text(bonItems[index].title),
-                    subtitle: Text(
-                        "Preis: " + bonItems[index].price.toString() + "€"),
-                    onTap: () {
-                      bonItems[index].setPayer(
-                          ref.read(payerNotifierProvider).selectedPayer);
-                      setState(() {});
-                    }))),
-          ),
           Expanded(
-              flex: 4,
-              child: Text(
-                "Summe: " + sum.toString() + "€",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              )),
+            flex: 3,
+            child: ListView.builder(
+                itemCount: bonItems.length + 1,
+                itemBuilder: ((context, index) {
+                  if (index == bonItems.length) {
+                    return Text(
+                      "Summe: $sumInEuros",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    );
+                  } else {
+                    return ListTile(
+                        trailing: Text(bonItems[index].payer ?? "Niemand"),
+                        title: Text(bonItems[index].title),
+                        // Number format is used to format the items price in euros
+                        subtitle: Text("Preis: " +
+                            NumberFormat.currency(locale: 'eu', symbol: "€")
+                                .format(bonItems[index].price)),
+                        onTap: () {
+                          bonItems[index].setPayer(selectedPayer);
+                          didChangePayer = true;
+                          setState(() {});
+                        });
+                  }
+                })),
+          ),
+          if (didChangePayer)
+            ElevatedButton(
+              onPressed: () {
+                didChangePayer = false;
+                setState(() {});
+              },
+              child: Text("Save"),
+            ),
           const Expanded(
-            flex: 2,
+            flex: 1,
             child: PayerListWidget(),
           )
         ],
